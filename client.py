ec70a538316b490d2b068f3634b5df54c8f824c4
yegor <yegor47@bk.ru>
2021-10-05 13:46:06 +0300

    Final ver

--- client.py
+++ client.py
@@ -1,10 +1,22 @@
 import socket
 import uuid
 
+# По порту 8000 передаем ID и получаем ещё один
 client_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client_id = uuid.uuid4().bytes
 client_sock.connect(('127.0.0.1', 8000))
-iden = uuid.uuid4()
-client_sock.send(bytes(iden))
-code = repr(client_sock.recv(1024))
+client_sock.sendall(bytes(client_id))
+cid = client_sock.recv(1024)
 client_sock.close()
-print('Received', code)
+
+# По порту 8001 передаем сообщение указанным способом
+client_sock1 = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client_sock1.connect(('127.0.0.2', 8001))
+print('Введите сообщение, котрое будет передано на сервер:')
+message = str(input())
+client_sock1.sendall(bytes(cid))
+client_sock1.sendall(bytes(client_id))
+client_sock1.sendall(bytes(message, encoding='utf8'))
+data = client_sock1.recv(1024)
+client_sock1.close()
+print('Received', repr(data))
--- server.py
+++ server.py
@@ -1,72 +1,66 @@
 import socket
-import sys
-import time
+import os
 
+# Открываем на прослушивание два нужных порта
+log = 'logfile.txt'
+serv_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM, proto=0)
+serv_sock.bind(('', 8000))
+serv_sock.listen(50)
 
-def run_server(port=8000):
-    serv_sock = create_serv_sock(port)
-    cid = 0
-    while True:
-        client_sock = accept_client_conn(serv_sock, cid)
-        serve_client(client_sock, cid)
+serv_sock1 = socket.socket(socket.AF_INET, socket.SOCK_STREAM, proto=0)
+serv_sock1.bind(('', 8001))
+serv_sock1.listen(50)
+
+checklist = []
+cid = 1
+
+while True:
+    # При прихождении данных на нужный порт соответственно их обрабатываем
+    serv_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM, proto=0)
+    serv_sock.bind(('', 8000))
+    serv_sock.listen(10)
+    # Бесконечно обрабатываем входящие подключения
+    client_sock, client_addr = serv_sock.accept()
+    print('Connected by', client_addr, ' on port 8000')
+
+    if client_addr != '':
+        while True:
+            # Пока клиент не отключился, читаем передаваемые
+            # им данные и отправляем обратно cid
+            data = client_sock.recv(1024)
+            if not data:
+                # Клиент отключился
+                break
+            client_sock.sendall(bytes(cid))
+            checklist.append(data)
+        client_sock.close()
+        client_addr = ''
         cid += 1
 
-
-def serve_client(client_sock, cid):
-    request = read_request(client_sock)
-    if request is None:
-        print(f'Client #{cid} unexpectedly disconnected')
-    else:
-        response = handle_request(request)
-        write_response(client_sock, response, cid)
-
-
-def create_serv_sock(serv_port):
-    serv_sock = socket.socket(socket.AF_INET,
-                              socket.SOCK_STREAM,
-                              proto=0)
-    serv_sock.bind(('', serv_port))
-    serv_sock.listen()
-    return serv_sock
-
-
-def accept_client_conn(serv_sock, cid):
-    client_sock, client_addr = serv_sock.accept()
-    print(f'Client #{cid} connected '
-          f'{client_addr[0]}:{client_addr[1]}')
-    return client_sock
-
-
-def read_request(client_sock, delimiter=b'!'):
-    request = bytearray()
-    try:
+    client_sock1, client_addr1 = serv_sock1.accept()
+    print('Connected by', client_addr1, ' on port 8001')
+    if client_addr1 != '':
+        received = []
         while True:
-            chunk = client_sock.recv(4)
-            if not chunk:
-                # Клиент преждевременно отключился.
-                return None
-
-            request += chunk
-            if delimiter in request:
-                return request
-
-    except ConnectionResetError:
-        # Соединение было неожиданно разорвано.
-        return None
-    except:
-        raise
-
-
-def handle_request(request):
-    time.sleep(5)
-    return request[::-1]
-
-
-def write_response(client_sock, response, cid):
-    client_sock.sendall(response)
-    client_sock.close()
-    print(f'Client #{cid} has been served')
-
-
-if __name__ == '__main__':
-    run_server(port=8000)
+            # Пока клиент не отключился, читаем передаваемые
+            # им данные и отправляем результат их обработки обратно
+            data = client_sock1.recv(1024)
+            if not data:
+                # Клиент отключился
+                break
+            received.append(data)
+            if len(received) == 3:
+                if checklist[0] == received[1]:
+                    client_sock1.sendall(b'Your message was received')
+                    if os.path.exists(str(log)):
+                        txt = open(str(log), "a")
+                    else:
+                        txt = open(str(log), "w+")
+                    txt.write('\nПринято от клиента с ID ' + str(received[1]) +
+                              ':\n' + str(received[2]))
+                    txt.close()
+                else:
+                    client_sock1.sendall(b'Data is illegal due to wrong ID')
+        print('Closing connection')
+        client_sock1.close()
+        client_addr1 = ''
